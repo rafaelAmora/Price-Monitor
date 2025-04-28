f67d0766887d577656ee1c964a36f1d4b36cd92d
rafaelAmora <rmoreira2209@gmail.com>
2025-04-28 17:59:14 -0300

    alter server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 
-const PORT = process.env.PORT;
+const PORT = Number(process.env.PORT);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("server is runnig on port " + PORT);
 });
